17da8f71ea79b3ff47a5d6b1ae931139b1aee427
jmayanja-xyz <51414069+jmayanja-xyz@users.noreply.github.com>
2022-06-22 21:21:35 +0100

    Redesigned accordion css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
-import './App.css';
-import Accordion from './Accordion';
+import Accordion from "./Accordion";
+import "./App.css";
 
 
-function App() {
+const App = () => {
+  const accordionData = [
+    {
+      title: "Step 1: Your details"
+    },
+    {
+      title: "Step 2: More comments"
+    },
+    {
+      title: "Step 3: Final Comments"
+    },
+  ];
+
   return (
     <div className="App">
-      <Accordion title="Step 1: Your details"/>
-      <Accordion title="Step 2: More comments"/>
-      <Accordion title="Step 3: Final comments"/>
+      <div className="accordion">
+       {accordionData.map(({ title }) => (
+  <Accordion title={title} />
+))}
+
+      </div>
     </div>
   );
+};
+
+
+
+
+{
+  /* <Accordion title="Step 1: Your details" />
+      <Accordion title="Step 2: More comments" />
+      <Accordion title="Step 3: Final comments" /> */
 }
 
 export default App;
