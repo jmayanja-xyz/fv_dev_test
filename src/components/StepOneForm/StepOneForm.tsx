ca4e0c577d28f18e96dff7c9b7a4a0dbbae09c87
jmayanja-xyz <51414069+jmayanja-xyz@users.noreply.github.com>
2022-06-23 00:55:31 +0100

    Styled form textboxes

--- src/components/StepOneForm/StepOneForm.tsx
+++ src/components/StepOneForm/StepOneForm.tsx
@@ -18,55 +18,66 @@ const StepOneForm = () => {
   };
 
   return (
-    <div className="Register">
+    <div className="register">
       <form onSubmit={handleSubmit(onSubmit)}>
-        <label>
-          First Name
-          <input
-            type="text"
-            {...register("firstname", {
-              required: "Please enter your first name.",
-              pattern: {
-                value: /^[a-zA-Z]+$/,
-                message: "Please enter a valid first namae",
-              },
-            })}
-          />
-        </label>
-        {errors.firstname && (
-          <div className="error-message">Valid firstname is required.</div>
-        )}
 
-        <label>
-          Surname
-          <input
-            type="text"
-            {...register("surname", {
-              required: "Please enter your first name.",
-            })}
-          />
-        </label>
-        {errors.surname && (
-          <div className="error-message">Valid surname is required.</div>
-        )}
 
-        <label>
-          Email Address:
-          <input
-            type="email"
-            {...register("email", {
-              required: "Please enter your valid email.",
-              pattern: {
-                value:
-                  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                message: "Please enter a valid email",
-              },
-            })}
-          />
+        <label className="first-name-label"> 
+              <div>First Name</div>
+            <input
+              type="text"
+              {...register("firstname", {
+                required: "Please enter your first name.",
+                pattern: {
+                  value: /^[a-zA-Z]+$/,
+                  message: "Please enter a valid first namae",
+                },
+              })}
+            />
+          
+          {errors.firstname && (
+            <div className="error-message">Valid firstname is required.</div>
+          )}
         </label>
-        {errors.email && (
-          <div className="error-message">Valid e-mail is required.</div>
-        )}
+
+        <div className="suname-textbox">
+          <label>
+            Surname
+            <input
+              type="text"
+              {...register("surname", {
+                required: "Please enter your first name.",
+              })}
+            />
+          </label>
+          {errors.surname && (
+            <div className="error-message">Valid surname is required.</div>
+          )}
+        </div>
+
+
+        <div className="email-textbox">
+          <label>
+            Email Address:
+            <input
+              type="email"
+              {...register("email", {
+                required: "Please enter your valid email.",
+                pattern: {
+                  value:
+                    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                  message: "Please enter a valid email",
+                },
+              })}
+            />
+          </label>
+
+          {errors.email && (
+            <div className="error-message">Valid e-mail is required.</div>
+          )}
+        </div>
+
+
 
         <button className="next-button">Next &gt;</button>
       </form>
